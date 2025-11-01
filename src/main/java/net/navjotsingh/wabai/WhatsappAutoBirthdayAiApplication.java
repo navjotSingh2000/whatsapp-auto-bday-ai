11a88813c0bfc63ec7f0ff7b2f2ccd5be59ed49a
navjotSingh2000 <abcnavjot2000@gmail.com>
2025-11-01 16:47:31 -0400

    chore: initial commit

---
+++ src/main/java/net/navjotsingh/wabai/WhatsappAutoBirthdayAiApplication.java
@@ -0,0 +1,13 @@
+package net.navjotsingh.wabai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappAutoBirthdayAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappAutoBirthdayAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/navjotsingh/wabai/WhatsappAutoBirthdayAiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.navjotsingh.wabai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappAutoBirthdayAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
