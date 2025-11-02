0c0a52392730f559f7fb2daed7068a97bde19d89
navjotSingh2000 <abcnavjot2000@gmail.com>
2025-11-02 01:02:49 -0400

    feat: added playwright; able to retrieve the linking code

--- src/main/java/net/navjotsingh/wabai/jobs/BirthdayCheckJob.java
+++ src/main/java/net/navjotsingh/wabai/jobs/BirthdayCheckJob.java
@@ -1,11 +1,13 @@
 package net.navjotsingh.wabai.jobs;
 
+import net.navjotsingh.wabai.model.Birthday;
 import net.navjotsingh.wabai.service.AutomationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class BirthdayCheckJob {
@@ -18,6 +20,8 @@ public class BirthdayCheckJob {
 
     @Scheduled(cron = "0 0 9 * * ?")  // run every day at 9 AM
     public void runJob() {
+        List<Birthday> list = automationService.findBirthdays();
+        System.out.println(list);
         automationService.createSession();
     }
 }
